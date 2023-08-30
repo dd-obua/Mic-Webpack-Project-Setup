aab37d0a9b3456f55984bc38d163c290f692e4a3
Obua Denis Daniel <dd.obua@gmail.com>
2023-08-30 19:53:41 +0300

    Add module rules for css-loader and style loader

--- webpack.config.js
+++ webpack.config.js
@@ -12,4 +12,10 @@ module.exports = {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+    rules: {
+      test: /\.css$/i,
+      use: ['style-loader', 'css-loader'],
+    },
+  },
 };
